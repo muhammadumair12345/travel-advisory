6f5764bb9b5a19a158ba4950685d6ed20f4a30aa
Muhammad Umair <hafizumairtariq1234@gmail.com>
2022-02-07 20:34:17 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <Header />
+            <List />
+            <Map />
+        </div>
+    );
 };
 
 export default App;
