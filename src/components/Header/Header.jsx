5e183fe8c0189a4b2ed7622db46b84499d882d51
Muhammad Umair <hafizumairtariq1234@gmail.com>
2022-02-07 21:05:38 +0500

    updated

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material';
+import React from 'react';
+import { HeaderContainer } from './Header.style';
+
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <Button/>
+        </HeaderContainer>
+    );
+};
+
+export default Header;
